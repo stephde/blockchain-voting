7c1259d115845928ec928228e0d23297687cf8af
Stephan Detje <stephan.detje@student.hpi.de>
2017-12-06 11:38:01 +0100

    add pseudo implementation flow

--- node-server/hyperledger/main.js
+++ node-server/hyperledger/main.js
@@ -13,9 +13,30 @@ function run() {
     let hlAdaper = init.initFabricClient(host, channelId);
 
     //sample usage of query execution
-    let queryPromise = query.executeQuery(hlAdaper.client, hlAdaper.channel, 'fabcar', 'queryAllCars', [''])
+    // let queryPromise = query.executeQuery(hlAdaper.client, hlAdaper.channel, 'fabcar', 'queryAllCars', [''])
     //sample usage of transaction invocation
-    let transactionPromise = transaction.invokeTransaction(hlAdaper.client, hlAdaper.channel, 'fabcar', '', 'mychannel', [''])
+    // let transactionPromise = transaction.invokeTransaction(hlAdaper.client, hlAdaper.channel, 'fabcar', '', 'mychannel', [''])
+
+    registerParties(["Green", "Blue", "Yellow", "Red"])
+        .then(() => registerEligibleVoters(["voter1", "voter2"]))
+        .then(() => configureVoting(new Date(), new Date().setHours(new Date().getHours() + 8)))
+        .then(() => enableVote())
+}
+
+function registerParties(parties) {
+    return Promise.resolve(() => true)
+}
+
+function registerEligibleVoters(parties) {
+    return Promise.resolve(() => true)
+}
+
+function configureVoting(startTime, endTime) {
+    return Promise.resolve(() => true)
+}
+
+function enableVote() {
+    return Promise.resolve(() => true)
 }
 
 run();
