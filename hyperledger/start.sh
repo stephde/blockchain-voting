87e4c2de6208ef228aca17f77b0a13db251617b6
Sven Lehmann <sven.lehmann93@gmail.com>
2017-12-06 10:35:15 +0100

    Fixes for setup

--- hyperledger/start.sh
+++ hyperledger/start.sh
@@ -5,20 +5,37 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Exit on first error
-set -e
+set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 LANGUAGE=${1:-"golang"}
-CC_SRC_PATH=lehmann.com/voting/go
+CC_SRC_PATH=hpi.de/go
 
 # clean the keystore
 rm -rf ./hfc-key-store
 
-# launch network; create channel and join peer to channel
-cd ../../fabric-test/fabric-samples/basic-network/
-./start.sh
+################ generate.sh #####################
+
+
+################ start.sh #####################
+
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=10
+#echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+# Create the channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+# Join peer0.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+
 
 # Now launch the CLI container in order to install, instantiate chaincode
 docker-compose -f ./docker-compose.yml up -d cli
--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 # From http://hyperledger-fabric.readthedocs.io/en/latest/samples.html
+# Installs the Hyperledger Sample Projects
 
-mkdir fabric-test && cd fabric-test
-git clone -b master https://github.com/hyperledger/fabric-samples.git
 mkdir platform-specific-binaries
 cd platform-specific-binaries
-curl -sSL https://goo.gl/fMh2s3 | bash
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/release/scripts/bootstrap-1.0.4.sh | bash
 export PATH=$(pwd)/bin:$PATH
-cd ../fabric-samples/basic-network
-./generate.sh
-./start.sh
-./stop.sh
+echo $PATH
+cd ../
