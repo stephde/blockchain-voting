6255449f80602fd67a7df7456cd03368ef0f6924
Sven Lehmann <sven.lehmann93@gmail.com>
2017-12-08 01:04:51 +0100

    Adding README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# blockchain-voting
+# Blockchain-voting
+
+## Setting up Hyperledger Network and run basic demo
+1. Run setup.sh to install platform-specific Docker images
+2. Run hyperledger/start.sh to start Docker containers and Hyperledger network
+3. Run hyperledger/demo_for_voting.sh to execute some transactions via CLI
--- demo_for_fabcar.sh
+++
@@ -1,6 +0,0 @@
-# must be run in cli docker container
-
-peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"Function":"changeCarOwner", "Args":["CAR4", "Max"]}'
-peer chaincode query -C mychannel -n fabcar -c '{"Function":"queryCar","Args":["CAR4"]}'
-peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"Function":"changeCarOwner", "Args":["CAR4", "user1"]}'
-peer chaincode query -C mychannel -n fabcar -c '{"Function":"queryCar","Args":["CAR4"]}'
