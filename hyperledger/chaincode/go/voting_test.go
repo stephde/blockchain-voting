e4428e32bfc622069e752737ac6ac93074555876
Sven Lehmann <sven.lehmann93@gmail.com>
2017-12-19 16:26:01 +0100

    Adding configuration of vote in chaincode

---
+++ hyperledger/chaincode/go/voting_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+func checkInit(t *testing.T, stub *shim.MockStub, args [][]byte) {
+	res := stub.MockInit("1", args)
+	if res.Status != shim.OK {
+		fmt.Println("Init failed", string(res.Message))
+		t.FailNow()
+	}
+}
+
+func checkState(t *testing.T, stub *shim.MockStub, name string, value string) {
+	bytes := stub.State[name]
+	if bytes == nil {
+		fmt.Println("State", name, "failed to get value")
+		t.FailNow()
+	}
+	if string(bytes) != value {
+		fmt.Println("State value", name, "was not", value, "as expected")
+		t.FailNow()
+	}
+}
+
+func checkQuery(t *testing.T, stub *shim.MockStub, name string, value string) {
+	res := stub.MockInvoke("1", [][]byte{[]byte("query"), []byte(name)})
+	if res.Status != shim.OK {
+		fmt.Println("Query", name, "failed", string(res.Message))
+		t.FailNow()
+	}
+	if res.Payload == nil {
+		fmt.Println("Query", name, "failed to get value")
+		t.FailNow()
+	}
+	if string(res.Payload) != value {
+		fmt.Println("Query value", name, "was not", value, "as expected")
+		t.FailNow()
+	}
+}
+
+func checkInvoke(t *testing.T, stub *shim.MockStub, args [][]byte) {
+	res := stub.MockInvoke("1", args)
+	if res.Status != shim.OK {
+		fmt.Println("Invoke", args, "failed", string(res.Message))
+		t.FailNow()
+	}
+}
+
+func Test_Query(t *testing.T) {
+	scc := new(SimpleChaincode)
+	stub := shim.NewMockStub("ex02", scc)
+
+	// Init A=345 B=456
+	checkInit(t, stub, [][]byte{[]byte("init"), []byte("A"), []byte("345"), []byte("B"), []byte("456")})
+
+	// Query A
+	checkQuery(t, stub, "A", "345")
+
+	// Query B
+	checkQuery(t, stub, "B", "456")
+}
