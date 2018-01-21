7d5b9ef39c63d568db1df6443d88613436c57bb7
Sven Lehmann <sven.lehmann93@gmail.com>
2018-01-21 13:22:04 +0100

    Implemeting YesVoteZKP

--- hyperledger/chaincode/go/crypto_test.go
+++ hyperledger/chaincode/go/crypto_test.go
@@ -48,6 +48,84 @@ func generateKeyPair() (pubkey *ecdsa.PublicKey, privkey *ecdsa.PrivateKey) {
 	return &key.PublicKey, key
 }
 
+func (s *SmartContract) create1outof2ZKPYesVote(
+	xG *ecdsa.PublicKey,
+	yG *ecdsa.PublicKey,
+	w *big.Int,
+	r1 *big.Int,
+	d1 *big.Int,
+	x *big.Int) ([]*big.Int, []*big.Int) {
+	// Return values
+	var res []*big.Int
+	var res2 []*big.Int
+
+	// Curve
+	curve := crypto.S256()
+
+	// Curve base point
+	Gx := curve.Params().Gx
+	Gy := curve.Params().Gy
+
+	var temp []*big.Int
+	var temp1 []*big.Int
+	var temp2 []*big.Int
+
+	// y = h^{x} * g
+	temp1[0], temp1[1] = curve.ScalarMult(yG.X, yG.Y, x.Bytes())
+	temp1[0], temp1[1] = curve.Add(temp1[0], temp1[1], Gx, Gy)
+	res[0] = temp1[0]
+	res[1] = temp1[1]
+
+	// a1 = g^{r1} * x^{d1}
+	temp1[0], temp1[1] = curve.ScalarMult(Gx, Gy, r1.Bytes())
+	temp2[0], temp2[1] = curve.ScalarMult(xG.X, xG.Y, d1.Bytes())
+	temp1[0], temp1[1] = curve.Add(temp1[0], temp1[1], temp2[0], temp2[1])
+	res[2] = temp1[0]
+	res[3] = temp1[1]
+
+	// b1 = h^{r1} * y^{d1} (temp = affine 'y')
+	temp1[0], temp1[1] = curve.ScalarMult(yG.X, yG.Y, r1.Bytes())
+
+	// Setting temp to 'y'
+	temp[0] = res[0]
+	temp[1] = res[1]
+
+	temp2[0], temp2[1] = curve.ScalarMult(temp[0], temp[1], d1.Bytes())
+	temp1[0], temp1[1] = curve.Add(temp1[0], temp1[1], temp2[0], temp2[1])
+	res[4] = temp1[0]
+	res[5] = temp1[1]
+
+	// a2 = g^{w}
+	temp1[0], temp1[1] = curve.ScalarMult(Gx, Gy, w.Bytes())
+	res[6] = temp1[0]
+	res[7] = temp1[1]
+
+	// b2 = h^{w} (where h = g^{y})
+	temp1[0], temp1[1] = curve.ScalarMult(yG.X, yG.Y, w.Bytes())
+	res[8] = temp1[0]
+	res[9] = temp1[1]
+
+	// Get c = H(id, xG, Y, a1, b1, a2, b2);
+	// id is H(round, voter_index, voter_address, contract_address)...
+	data := Append([]byte("")[:], xG.X, xG.Y, res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[8])
+	hashBytes := sha256.Sum256(data)
+	c := new(big.Int)
+	c.SetBytes(hashBytes[:])
+
+	// d2 = c - d1 mod q
+	n := curve.Params().N
+	temp[0] = subMod(c, d1, n)
+	// r2 = w - (x * d2)
+	temp[1] = subMod(w, mulMod(x, temp[0], n), n)
+
+	res2[0] = d1
+	res2[1] = temp[0]
+	res2[2] = r1
+	res2[3] = temp[1]
+
+	return res, res2
+}
+
 func Test_verifyZKP(t *testing.T) {
 	publicKeyECDSA, privateKeyECDSA := generateKeyPair()
 	userID := "someUserId"
