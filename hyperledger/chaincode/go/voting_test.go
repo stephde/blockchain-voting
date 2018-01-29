0653b82bb7cbbf30d08181d953e2c34bfcdbe3b6
Sven Lehmann <sven.lehmann93@gmail.com>
2018-01-29 15:18:32 +0100

    Cleanup

--- hyperledger/chaincode/go/voting_test.go
+++ hyperledger/chaincode/go/voting_test.go
@@ -67,18 +67,9 @@ func Test_InvalidFunctionName(t *testing.T) {
 }
 
 func Test_End2End_Test(t *testing.T) {
-	// runFuncParallelForUsers((userId) => hyperledger.registerUser({id: userId}), userIds)
-	// 		.then(() => timedCall(hyperledger.initVote, [], 'Init Vote'))
-	// 		.then(() => timedCall(hyperledger.setEligible, userIds, 'Set Eligible'))
-	// 		.then(() => timedCall(hyperledger.beginSignUp, "Do you like Blockchain?", 'begin sign up'))
-	// 		.then(() => timedCall(() => runFuncParallelForUsers((userId) => hyperledger.registerForVote(userId), userIds), [], 'register for vote'))
-	// 		.then(() => timedCall(hyperledger.beginVote, [], 'begin vote'))
-	// 		.then(() => timedCall(() => runFuncParallelForUsers((userId) => hyperledger.vote(userId, 0), [], 'voting')))
-	// 		.then(() => timedCall(hyperledger.computeTally, [], "compute tally"))
-	// 		.then(console.log, console.log)
-	userId1 := "a"
-	userId2 := "b"
-	userId3 := "c"
+	userID1 := "a"
+	userID2 := "b"
+	userID3 := "c"
 
 	stub := shim.NewMockStub("test_invalid_question", new(SmartContract))
 	// init vote
@@ -86,25 +77,25 @@ func Test_End2End_Test(t *testing.T) {
 
 	// Set eligible users
 	checkInvoke(t, stub, [][]byte{[]byte("setEligible"),
-		[]byte(userId1),
-		[]byte(userId2),
-		[]byte(userId3)})
+		[]byte(userID1),
+		[]byte(userID2),
+		[]byte(userID3)})
 
 	// Begin SignUp
 	checkInvoke(t, stub, [][]byte{[]byte("beginSignUp"), []byte("Do you like Blockchain?")})
 
 	// Register users
-	checkInvoke(t, stub, [][]byte{[]byte("register"), []byte(userId1)})
-	checkInvoke(t, stub, [][]byte{[]byte("register"), []byte(userId2)})
-	checkInvoke(t, stub, [][]byte{[]byte("register"), []byte(userId3)})
+	checkInvoke(t, stub, [][]byte{[]byte("register"), []byte(userID1)})
+	checkInvoke(t, stub, [][]byte{[]byte("register"), []byte(userID2)})
+	checkInvoke(t, stub, [][]byte{[]byte("register"), []byte(userID3)})
 
 	// Begin Vote
 	checkInvoke(t, stub, [][]byte{[]byte("finishRegistrationPhase")})
 
 	// Vote
-	checkInvoke(t, stub, [][]byte{[]byte("submitVote"), []byte(userId1), []byte(strconv.Itoa(0))})
-	checkInvoke(t, stub, [][]byte{[]byte("submitVote"), []byte(userId2), []byte(strconv.Itoa(1))})
-	checkInvoke(t, stub, [][]byte{[]byte("submitVote"), []byte(userId3), []byte(strconv.Itoa(1))})
+	checkInvoke(t, stub, [][]byte{[]byte("submitVote"), []byte(userID1), []byte(strconv.Itoa(0))})
+	checkInvoke(t, stub, [][]byte{[]byte("submitVote"), []byte(userID2), []byte(strconv.Itoa(1))})
+	checkInvoke(t, stub, [][]byte{[]byte("submitVote"), []byte(userID3), []byte(strconv.Itoa(1))})
 
 	// Compute computeTally
 	expectedResult, _ := json.Marshal(Result{3, map[int]int{0: 1, 1: 2}})
