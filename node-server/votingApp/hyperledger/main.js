89a620b2b685a79c892b328bc166343e438d92a6
Sven Lehmann <sven.lehmann93@gmail.com>
2018-01-31 00:30:10 +0100

    Fixes for benchmark

--- node-server/votingApp/hyperledger/main.js
+++ node-server/votingApp/hyperledger/main.js
@@ -6,7 +6,7 @@ let Hyperledger = require("./hyperledger.js");
 let hyperledger = new Hyperledger();
 
 function runElection() {
-    const numOfUsers = 10;
+    const numOfUsers = 30;
 
     let userIds = []
     for (let i=0; i < numOfUsers; i++) {
@@ -18,10 +18,25 @@ function runElection() {
         .then(() => timedCall(hyperledger.setEligible, userIds, 'Set Eligible'))
         .then(() => timedCall(hyperledger.beginSignUp, "Do you like Blockchain?", 'begin sign up'))
         .then(() => timedCall(() => runFuncParallelForUsers((userId) => hyperledger.registerForVote(userId), userIds), [], 'register for vote'))
-        .then(() => timedCall(hyperledger.finishRegistrationPhase, [], 'finishRegistrationPhase'))
-        .then(() => timedCall(() => runFuncParallelForUsers((userId) => hyperledger.vote(userId, '0'), userIds), [], 'voting'))
-        .then(() => timedCall(hyperledger.computeTally, [], "compute tally"))
-        .then(console.log, console.log)
+        .then(() => setTimeout(function() {
+          console.log("Waited for 2 seconds");
+          timedCall(hyperledger.finishRegistrationPhase, [], 'finishRegistrationPhase')
+          .then(() => timedCall(() => runFuncParallelForUsers((userId) => hyperledger.vote(userId, '1'), userIds), [], 'voting'))
+          .then(() => setTimeout(function() {
+            console.log("Waited for 5 seconds");
+            timedCall(hyperledger.computeTally, [], "compute tally")
+          }, 5000))
+          // .then(() => timedCall(hyperledger.computeTally, [], "compute tally"))
+          .then(console.log, console.log)
+        }, 2000))
+        // .then(() => timedCall(hyperledger.finishRegistrationPhase, [], 'finishRegistrationPhase'))
+        // .then(() => timedCall(() => runFuncParallelForUsers((userId) => hyperledger.vote(userId, '1'), userIds), [], 'voting'))
+        // .then(() => setTimeout(function() {
+        //   console.log("Waited for 1 seconds");
+        //   timedCall(hyperledger.computeTally, [], "compute tally")
+        // }, 1000))
+        // // .then(() => timedCall(hyperledger.computeTally, [], "compute tally"))
+        // .then(console.log, console.log)
 }
 
 function timedCall(func, params, identifier){
