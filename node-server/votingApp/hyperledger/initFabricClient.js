1c8e420c0470489c8036e4f6a947024d6569d4ea
Stephan Detje <stephan.detje@student.hpi.de>
2018-01-31 15:24:09 +0100

    introduce single global event hub, instead of creating a new one on every transaction

--- node-server/votingApp/hyperledger/initFabricClient.js
+++ node-server/votingApp/hyperledger/initFabricClient.js
@@ -10,15 +10,23 @@ exports.initFabricClient = function (host, channelId) {
     // setup the fabric network
     let channel = fabricClient.newChannel(channelId); //
     let peer = fabricClient.newPeer(host);
+    // peer.setName("peerio")
+    // console.log('####################\n\n Setting peer name to - ' + peer.getName() + '\n')
     channel.addPeer(peer);
     let order = fabricClient.newOrderer('grpc://localhost:7050')
     channel.addOrderer(order);
+
+    // get an eventhub once the fabric client has a user assigned. The user
+    // is required bacause the event registration must be signed
+    let eventHub = fabricClient.newEventHub();
+    eventHub.setPeerAddr('grpc://localhost:7053');
 
 
     let clientWrapper = {
         client: fabricClient,
         channel: channel,
-        peer: peer
+        peer: peer,
+        eventHub: eventHub
     };
 
     console.log("Fabric client has been initialized with : ", clientWrapper);
