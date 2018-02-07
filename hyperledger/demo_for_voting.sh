1efe07586a485872f2c1cfb362284120d7b5ea40
Sven Lehmann <sven.lehmann93@gmail.com>
2018-02-08 00:12:12 +0100

    Some tweaks for benchmark

--- hyperledger/demo_for_voting.sh
+++ hyperledger/demo_for_voting.sh
@@ -5,14 +5,14 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 run_register() {
   for (( i = 1; i <= $1; ++i));
   do
-    docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"register", "Args":["'$i'"]}' &
+    docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"register", "Args":["'$i'"]}' &> out.log &
   done
 }
 
 run_voting() {
   for ((i=1;i<=$1;i++));
   do
-    docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"submitVote", "Args":["'$i'", "1"]}'
+    docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"submitVote", "Args":["'$i'", "1"]}' &> out.log &
   done
 }
 
@@ -20,20 +20,18 @@ run_benchmark() {
   numberOfUsers=$1
 
   printf "############### Init vote \n"
-  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"initVote", "Args":[]}' &
+  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"initVote", "Args":[]}' &> out.log
   sleep 5
 
   userIDs=$(echo '"'$(seq -s '","' $numberOfUsers))
   userIDs=${userIDs%??};
 
-  printf $userIDs"\n"
-
   printf "############### Set eligible \n"
-  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"setEligible", "Args":['$userIDs']}' &
+  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"setEligible", "Args":['$userIDs']}' &> out.log
   sleep 3
 
   printf "############### Begin Signup \n"
-  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"beginSignUp", "Args":["Do you like Blockchain?"]}' &
+  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"beginSignUp", "Args":["Do you like Blockchain?"]}' &> out.log
   sleep 3
 
   printf "############### Register \n"
@@ -46,7 +44,7 @@ run_benchmark() {
   sleep 10
 
   printf "############### Finish registration phase \n"
-  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"finishRegistrationPhase", "Args":[]}'
+  docker exec cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n vote -c '{"Function":"finishRegistrationPhase", "Args":[]}' &> out.log
   sleep 3
 
   printf "############### Vote \n"
@@ -62,4 +60,8 @@ run_benchmark() {
   docker exec cli peer chaincode query -C mychannel -n vote -c '{"Function":"computeTally","Args":[]}'
 }
 
-run_benchmark 100
+for i in 10 20 30 40 50 60 70 80 90 100 200
+do
+  printf "#### Running benchmark for "$i" ####\n"
+  run_benchmark $i
+done
