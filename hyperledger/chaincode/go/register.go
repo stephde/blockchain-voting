c42ee59d2503be8e9aab18fe3e639306b7a27209
Sven Lehmann <sven.lehmann93@gmail.com>
2018-02-07 13:47:03 +0100

    Further E2E test

--- hyperledger/chaincode/go/register.go
+++ hyperledger/chaincode/go/register.go
@@ -18,29 +18,40 @@ func (s *SmartContract) register(stub shim.ChaincodeStubInterface, args []string
 
 	var eligible map[string]bool
 	GetState(stub, "eligible", &eligible)
+	logger.Info("eligible {}", eligible)
 
 	var registered map[string]bool
 	GetState(stub, "registered", &registered)
+	logger.Info("registered {}", registered)
 
 	isEligible := eligible[userID]
 	isRegistered := registered[userID]
+
+	logger.Info("iseligible {}", isEligible)
+	logger.Info("isregistered {}", isRegistered)
 
 	voter := Voter{userID, -1}
 
 	if isEligible && !isRegistered {
 		registered[userID] = true
+		logger.Info("registered {}", registered)
 		PutState(stub, "registered", registered)
 
 		var totalRegistered int
 		GetState(stub, "totalRegistered", &totalRegistered)
 		totalRegistered++
+		logger.Info("totalRegistered {}", totalRegistered)
 		PutState(stub, "totalRegistered", totalRegistered)
 
 		var voters map[string]Voter
 		GetState(stub, "voters", &voters)
+		logger.Info("voters {}", voters)
 		voters[userID] = voter
+		logger.Info("voters {}", voters)
 		PutState(stub, "voters", voters)
+		return shim.Success(nil)
 	}
 
-	return shim.Success(nil)
+	return shim.Error("not eligible")
+
 }
