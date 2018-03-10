03ee32d1b20f32c8e4964e5ed3d7eda449859322
Sven Lehmann <sven.lehmann93@gmail.com>
2018-03-10 15:24:58 +0100

    Disable End2End benchmark

--- hyperledger/chaincode/go/end2end_test.go
+++ hyperledger/chaincode/go/end2end_test.go
@@ -10,7 +10,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_End2End(t *testing.T) {
+//func Test_End2End(t *testing.T) {
+func Ignore_End2End(t *testing.T) {
 	users := []int{10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000}
 
 	for _, numUser := range users {
